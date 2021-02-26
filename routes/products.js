5c6fa74a9b1e514cd17a01449f2062c24764da2c
Kainan Rodrigues <kainan.ars@gmail.com>
2021-02-25 22:11:12 -0300

    Meu primeiro desafio

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const productsModel = require("../models/products");
+const fs = require("fs");
 // const productsModel = require(COMPLETE_O_CAMINHO); TODO
 
 // GET - /products
@@ -14,11 +15,29 @@ router.get("/", function (req, res) {
 });
 
 // POST - /products
+function productsList(){
+  if (fs.existsSync('productsList.txt')){
+    return
+  }else{
+    return fs.writeFileSync('productsList.txt', '')
+  }};
+
 
 router.post("/", function (req, res) {
-  const newProduct = req.body;
+
+    var data = new Date()
+    var dia = data.getDate();
+    var mes = data.getMonth();
+    var ano = data.getFullYear();
+    var hour = data.getHours();
+    var min = data.getMinutes();
+    data = "Data: " + dia + '/' + (mes++) + '/' + ano + " Hora: " + hour + ':' + min;
+  
+const newProduct = req.body;
 
   productsModel.insertProduct(newProduct);
+  
+  fs.appendFileSync('productsList.txt', '\n' + "new log"+ '\n' + 'Produto: ' + newProduct.title + ' ' +  data + ' - ' +  'Operação de Cadastro');
   res.redirect("/products");
 });
 
